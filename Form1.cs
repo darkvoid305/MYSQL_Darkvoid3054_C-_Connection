d5bb1c0520e9451000e3f8c9388ba658f477ed00
darkvoid305 <darkvoid305@gmail.com>
2022-01-25 20:35:11 +0100

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using dataBaseConnectionTest.classes.Plugin;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows.Forms;
+
+namespace dataBaseConnectionTest
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void Register_Click(object sender, EventArgs e)
+        {
+            String data = Web.GetPost("http://127.0.0.1/Register.php", "UserName", "tim", "Password", "test");
+            Console.WriteLine(data);
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            String data = Web.GetPost("http://127.0.0.1/Login.php", "UserName", "daan", "Password", "test");
+            Console.WriteLine(data);
+        }
+    }
+}
---
+++ classes/Plugin/Web.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dataBaseConnectionTest.classes.Plugin
+{
+    class Web
+    {
+
+        public static string GetPost(string Url, params string[] postdata)
+        {
+            string result = string.Empty;
+            string data = string.Empty;
+
+            System.Text.ASCIIEncoding ascii = new ASCIIEncoding();
+
+            if(postdata.Length % 2 != 0 )
+            {
+
+                MessageBox.Show("Parameters must be even ",Application.ProductName,MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return string.Empty;
+            }
+
+            for(int i = 0; i < postdata.Length; i+=2)
+            {
+                data += string.Format("&{0}={1}",postdata[i],postdata[i+1]);
+            }
+
+            data = data.Remove(0, 1);
+            byte[] bytesarr = ascii.GetBytes(data);
+
+            try
+            {
+                WebRequest request = WebRequest.Create(Url);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = bytesarr.Length;
+
+                System.IO.Stream streamwritter = request.GetRequestStream();
+                streamwritter.Write(bytesarr,0,bytesarr.Length);
+                streamwritter.Close();
+
+                WebResponse response = request.GetResponse();
+                streamwritter = response.GetResponseStream();
+
+                System.IO.StreamReader streamread = new System.IO.StreamReader(streamwritter);
+                result = streamread.ReadToEnd();
+                streamread.Close();
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,Application.ProductName, MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+
+            return result;
+        }
+
+
+    }
+}
